bd739923cd778c13f9b2ef17a5db046fbdeb5971
Tomasz <tomeksz104@gmail.com>
2023-12-25 14:12:59 +0100

    feat: add next13-progressbar for loading progress indication

--- components/Provider.js
+++ components/Provider.js
@@ -2,10 +2,16 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
+import { Next13ProgressBar } from "next13-progressbar";
 
 const Provider = ({ children, session }) => (
   <SessionProvider session={session}>
     <ThemeProvider enableSystem={false}>{children}</ThemeProvider>
+    <Next13ProgressBar
+      color="#22c55e"
+      options={{ showSpinner: false }}
+      showOnShallow
+    />
   </SessionProvider>
 );
 
