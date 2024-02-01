56329419791505c20b9c81a208b6bf97f066cb4c
Tomasz <tomeksz104@gmail.com>
2024-02-01 20:55:03 +0100

    feat: Add error reporting dialog and update Prisma schema

---
+++ components/ErrorReportDialog.js
@@ -0,0 +1,104 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { useFormState } from "react-dom";
+import { useToast } from "@/hooks/useToast";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { insertErrorReport } from "@/actions/errorReportManager";
+
+const ErrorReportDialog = ({ isOpen, onClose }) => {
+  const pathname = usePathname();
+  const toast = useToast();
+  const initialState = { message: null, errors: {} };
+
+  const form = useForm({
+    defaultValues: {
+      content: undefined,
+    },
+  });
+
+  const [state, dispatch] = useFormState(insertErrorReport, initialState);
+
+  async function onSubmit(data) {
+    const formData = new FormData();
+
+    formData.append("pageUrl", pathname);
+
+    dispatch({ ...data, pageUrl: pathname });
+  }
+
+  useEffect(() => {
+    if (state.errors && state.message) {
+      toast.error(state.message);
+    } else if (state.message) {
+      form.setValue("content", "");
+      toast.success(state.message);
+    }
+  }, [state]);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Zgłoś błąd</DialogTitle>
+          <DialogDescription>
+            Tutaj możesz zgłosić napotkany błąd. Kliknij zapisz, kiedy
+            skończysz.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Informacje o napotkanym błędzie..."
+                      className="resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {state?.errors?.content &&
+              state.errors.content.map((error) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+
+            <DialogFooter className="mt-5">
+              <Button type="submit">Wyślij zgłoszenie</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ErrorReportDialog;
--- components/PlaceDetails/QuickListingActions.js
+++ components/PlaceDetails/QuickListingActions.js
@@ -11,49 +11,61 @@ import {
   Share2,
 } from "lucide-react";
 
-const QuickListingActions = () => {
-  return (
-    <ScrollArea className="w-full whitespace-nowrap ">
-      <div className="flex items-center space-x-3">
-        <Button
-          variant="outline"
-          className="flex items-center rounded-full hover:bg-white hover:border-gray-500"
-        >
-          <MessageCircle size={12} className="mr-2" />
-          Dodaj opinię
-        </Button>
-        <Button
-          variant="outline"
-          className="flex items-center rounded-full hover:bg-white hover:border-gray-500"
-        >
-          <Navigation size={12} className="mr-2" />
-          Nawiguj
-        </Button>
-        <Button
-          variant="outline"
-          className="flex items-center rounded-full hover:bg-white hover:border-gray-500"
-        >
-          <Phone size={12} className="mr-2" />
-          Zadzwoń
-        </Button>
-        <Button
-          variant="outline"
-          className="flex items-center rounded-full hover:bg-white hover:border-gray-500"
-        >
-          <Share2 size={12} className="mr-2" />
-          Udostępnij
-        </Button>
-        <Button
-          variant="outline"
-          className="flex items-center rounded-full hover:bg-white hover:border-gray-500"
-        >
-          <AlertCircle size={12} className="mr-2" />
-          Zgłoś błąd
-        </Button>
-      </div>
+import { useState } from "react";
+import ErrorReportDialog from "@/components/ErrorReportDialog";
 
-      <ScrollBar orientation="horizontal" />
-    </ScrollArea>
+const QuickListingActions = () => {
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
+  const toggleDialog = () => setDialogOpen(!isDialogOpen);
+
+  return (
+    <>
+      <ScrollArea className="w-full whitespace-nowrap ">
+        <div className="flex items-center space-x-3">
+          <Button
+            variant="outline"
+            className="flex items-center rounded-full hover:bg-white hover:border-gray-500"
+          >
+            <MessageCircle size={12} className="mr-2" />
+            Dodaj opinię
+          </Button>
+          <Button
+            variant="outline"
+            className="flex items-center rounded-full hover:bg-white hover:border-gray-500"
+          >
+            <Navigation size={12} className="mr-2" />
+            Nawiguj
+          </Button>
+          <Button
+            variant="outline"
+            className="flex items-center rounded-full hover:bg-white hover:border-gray-500"
+          >
+            <Phone size={12} className="mr-2" />
+            Zadzwoń
+          </Button>
+          <Button
+            variant="outline"
+            className="flex items-center rounded-full hover:bg-white hover:border-gray-500"
+          >
+            <Share2 size={12} className="mr-2" />
+            Udostępnij
+          </Button>
+          <Button
+            onClick={toggleDialog}
+            variant="outline"
+            className="flex items-center rounded-full hover:bg-white hover:border-gray-500"
+          >
+            <AlertCircle size={12} className="mr-2" />
+            Zgłoś błąd
+          </Button>
+        </div>
+
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+
+      <ErrorReportDialog isOpen={isDialogOpen} onClose={toggleDialog} />
+    </>
   );
 };
 export default QuickListingActions;
